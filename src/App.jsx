60a69faee4d161869e5665dc2b4d169b7a7b8271
nusrat <nusratchy.002@gmail.com>
2026-03-31 20:16:07 +0600

    states bar done

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
           </div>
         </div>
 
-        {/* RIGHT */}
+        
         <div className="flex justify-end">
           <img 
             src={bannerImg}
@@ -80,6 +80,32 @@ function App() {
 
       </div>
 
+
+<div className="w-full bg-gradient-to-r from-blue-600 via-purple-500 to-indigo-600 py-10">
+
+  <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 text-center text-white">
+
+
+    <div className="border-r border-white/20">
+      <h2 className="text-3xl font-bold">50K+</h2>
+      <p className="text-sm opacity-80">Active Users</p>
+    </div>
+
+   
+    <div className="border-r border-white/20">
+      <h2 className="text-3xl font-bold">200+</h2>
+      <p className="text-sm opacity-80">Premium Tools</p>
+    </div>
+
+    
+    <div>
+      <h2 className="text-3xl font-bold">4.9</h2>
+      <p className="text-sm opacity-80">Rating</p>
+    </div>
+
+  </div>
+
+</div>
     </div>
   )
 }
