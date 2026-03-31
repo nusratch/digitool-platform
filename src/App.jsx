10230b844ca812ecce5e2ac9eb11b848b2b06b53
nusrat <nusratchy.002@gmail.com>
2026-03-31 19:34:59 +0600

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import './App.css'
+import bannerImg from './assets/banner.png'
+
+function App() {
+  return (
+    <div>
+
+  
+      <div className="navbar bg-base-100 w-full px-16">
+
+        <div className="navbar-start">
+          <a className="text-xl font-bold text-blue-600">DigiTools</a>
+        </div>
+
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal gap-8 text-gray-600 font-medium">
+            <li><a>Products</a></li>
+            <li><a>Features</a></li>
+            <li><a>Pricing</a></li>
+            <li><a>Testimonials</a></li>
+            <li><a>FAQ</a></li>
+          </ul>
+        </div>
+
+        <div className="navbar-end gap-4">
+          <a className="text-gray-600">Login</a>
+          <button className="btn bg-blue-500 text-white rounded-full px-6">
+            Get Started
+          </button>
+        </div>
+
+      </div>
+
+     
+      <div className="w-full max-w-7xl mx-auto px-20 py-16 grid lg:grid-cols-2 gap-10 items-center">
+
+    
+        <div>
+          <p className="inline-block bg-purple-100 text-purple-600 px-4 py-1 rounded-full text-sm mb-4">
+            New AI-Powered Tools Available
+          </p>
+
+          <h1 className="text-4xl lg:text-5xl font-bold text-gray-800 leading-tight mb-6">
+            Supercharge Your <br />
+            Digital Workflow
+          </h1>
+
+          <p className="text-gray-500 mb-6">
+            Access premium AI tools, design assets, templates, and productivity 
+            software — all in one place. Start creating faster today.
+          </p>
+
+          <div className="flex gap-4">
+            <button className="btn bg-blue-500 text-white rounded-full px-6">
+              Explore Products
+            </button>
+
+            <button className="btn btn-outline rounded-full px-6">
+              ▶ Watch Demo
+            </button>
+          </div>
+        </div>
+
+        {/* RIGHT */}
+        <div className="flex justify-end">
+          <img 
+            src={bannerImg}
+            alt="banner"
+            className="w-full max-w-md lg:max-w-lg"
+          />
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css";
+import Nav from "./components/navbar/Nav";
+
+const Nav = () => {
+  return (
+<Nav/>
+  );
+};
+
+export default Nav;
